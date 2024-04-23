b23e9e1ffa6b6f26b9b4b75c99aeae2d7bfaef0f
JamesBong0070 <130417734+JamesBong0070@users.noreply.github.com>
2024-04-23 11:02:17 -0500

    Cleanup and bugfixes
    removed yes and no variables
    removed invalid NULL's
    fixed winner text
    added pauses after game ending choices

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -2,63 +2,66 @@
 
 class startInput {
 public:
-	const char yesInput = 'y';
-	const char noInput = 'n';
-	char userInput = NULL;
-	bool run = true;
-	void yesOrNo() { // Takes the user input, ensures it's valid, and sets it lowercase for compatibility.
+	char userInput = 0;
+	void yesOrNo() {   // Takes the user input, ensures it's valid, and sets it lowercase for compatibility.
+		bool response;
 		do {
 			std::cin >> userInput;
 			userInput = tolower(userInput);
-			if (userInput == noInput) {
-				std::cout << "You've entered 'n', the program will end now.";
-				run = false;
-				break;
+			if (userInput == 'n') {
+				std::cout << "\nYou've entered 'n', see ya!\n\n";
+				response = false;
+				system("pause");
 			}
-			if (userInput == yesInput) {
+			else if (userInput == 'y') {
 				std::cout << "You've entered 'y', your game will begin shortly...";
-				run = false;
-				break;
+				response = false;
 			}
 			else {
 				std::cout << "You've entered an invalid response, please type either 'y' or 'n'.\nThank you. (y\\n) ";
+				response = true;
 			}
-		} while (run);
+		} while (response == true);
 	}
 };
 
 class activeGame {  // Game: number generator and user input
 public:
-	int winnerNum = NULL;
-	int userInput = NULL;
+	int winnerNum = 0;
+	int userInput = 0;
 	const int randMax = 10;
 	const int randMin = 1;
+	bool winStatus = false;
 	void randomNum() {
 		srand(time(NULL));
 		int winnerNum = rand() % randMax + randMin;
-		std::cout << "The winning number is ";
+		std::cout << "\nThe winning number is ";
 		std::cout << winnerNum << " ";
+		if (winnerNum == userInput) {
+			winStatus = true;
+		}
 	}
 };
 
-int main() {
-	               //Game Start
+int main() {	//Game Start
 	std::cout << "Welcome to Trent's guessing game!\n";
 	std::cout << "Type 'y' to start playing or 'n' to quit. ";
 	startInput gameStart;
 	activeGame gameWorking;
 	gameStart.yesOrNo();
-	if (gameStart.userInput == gameStart.yesInput) {
+	if (gameStart.userInput == 'y') {
 		std::cout << "\n\nStarting Game...Please Wait";
 		std::cout << "\n\nGuess a number between 1 and 10: ";
 		std::cin >> gameWorking.userInput;
 		gameWorking.randomNum();
 		std::cout << "\nYou picked " << gameWorking.userInput << ".";
-		if (gameWorking.userInput == gameWorking.winnerNum) { // Checks if you're a winner or not
-			std::cout << "\nYou won, Congradulations!";
+		if (gameWorking.winStatus) { // Checks if you're a winner or not
+			std::cout << "\n\nYou won, Congradulations!\n\n";
+			system("pause");
 		}
 		else {
-			std::cout << "\nYou guessed incorrectly, better luck next time!";
+			std::cout << "\n\nYou guessed incorrectly, better luck next time!\n\n";
+			system("pause");
 		}
 	}
 	return 0;
